c534e0efeaf34b88aa831232d8daefefa239b1f7
zhengji <zhengji@stormorai.com>
2019-02-28 00:59:16 -0600

    first version

--- TimerCountDownApp/ViewController.swift
+++ TimerCountDownApp/ViewController.swift
@@ -11,45 +11,58 @@ import UIKit
 class ViewController: UIViewController {
     
     var seconds = 30
+    var add_time = 15
+    var cnt = 0
     var timer = Timer()
     
+    
     @IBOutlet weak var lblTime: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider1: UISlider!
+    @IBOutlet weak var slider2: UISlider!
+    @IBOutlet weak var timeExtension: UIButton!
     
-    @IBAction func slider(_ sender: UISlider) {
+    @IBOutlet weak var lblBasicTime: UILabel!
+    @IBOutlet weak var lblAddTime: UILabel!
+    
+    @IBAction func slider1(_ sender: UISlider) {
         seconds = Int(sender.value)
+        cnt = seconds
         lblTime.text = String(seconds)
+        lblBasicTime.text = "basic time:"+String(seconds)
     }
     
-    @IBAction func btnStart(_ sender: Any) {
+    
+    
+    @IBAction func slider2(_ sender: UISlider) {
+        add_time = Int(sender.value)
+        lblAddTime.text = "additional time:"+String(add_time)
+        
+    }
+    
+    
+    @IBAction func btnNext(_ sender: Any) {
+        cnt = seconds
+        lblTime.text = String(cnt)
+        if !timer.isValid{
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.counter), userInfo: nil, repeats: true)
+        }
     }
     
-    @IBAction func btnStop(_ sender: Any) {
-        timer.invalidate()
-        seconds = 30
-        slider.setValue(30, animated: true)
-        lblTime.text = "30"
+    @IBAction func btnExtension(_ sender: Any) {
+        cnt += add_time
+        lblTime.text = String(cnt)
     }
     
     @objc func counter() {
-        seconds -= 1
-        //lblTime.text = String(seconds)
-        lblTime.text = timeString(time: TimeInterval(seconds))
+        cnt -= 1
+        lblTime.text = String(cnt)
         
-        if (seconds == 0) {
+        if (cnt == 0) {
             timer.invalidate()
             Alert()
         }
-        slider.value = Float(seconds)
     }
     
-    func timeString(time:TimeInterval) -> String {
-        let hours = Int(time) / 3600
-        let minutes = Int(time) / 60 % 60
-        let seconds = Int(time) % 60
-        return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
-    }
     
     func Alert() {
         let alertController = UIAlertController(title: "Time's Up", message: "A standard alert.", preferredStyle: .alert)
